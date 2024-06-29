f98d20fd64eac281a6358af8a00c37848677bc15
Roberto Nares <roberto.nares@gmail.com>
2024-06-29 14:18:06 -0700

    Implement operate function that calls correct function for corresponding operator

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let operand1;
+let operand2;
+let operator;
+
+function operate(operator, operand1, operand2) {
+    if (operator === "+") return add(operand1, operand2);
+    if (operator === "-") return subtract(operand1, operand2);
+    if (operator === "*") return multiply(operand1, operand2);
+    if (operator === "/") return divide(operand1, operand2);
+}
+
 function add(operand1, operand2) {
     return operand1 + operand2;
 }
@@ -14,29 +25,3 @@ function divide(operand1, operand2) {
     if (operand2 === 0) return "Really bruh?";
     return operand1 / operand2;
 }
-
-console.log("Testing add()");
-console.log(`Expected: 7, Actual: ${add(3, 4)}`);
-console.log(`Expected: 1, Actual: ${add(-3, 4)}`);
-console.log(`Expected: -1, Actual: ${add(3, -4)}`);
-console.log(`Expected: -7, Actual: ${add(-3, -4)}`);
-
-console.log("Testing subtract()");
-console.log(`Expected: -1, Actual: ${subtract(3, 4)}`);
-console.log(`Expected: -7, Actual: ${subtract(-3, 4)}`);
-console.log(`Expected: 7, Actual: ${subtract(3, -4)}`);
-console.log(`Expected: 1, Actual: ${subtract(-3, -4)}`);
-
-console.log("Testing multiply()");
-console.log(`Expected: 12, Actual: ${multiply(3, 4)}`);
-console.log(`Expected: -12, Actual: ${multiply(-3, 4)}`);
-console.log(`Expected: -12, Actual: ${multiply(3, -4)}`);
-console.log(`Expected: 12, Actual: ${multiply(-3, -4)}`);
-
-console.log("Testing divide()");
-console.log(`Expected: 0.75, Actual: ${divide(3, 4)}`);
-console.log(`Expected: -0.75, Actual: ${divide(-3, 4)}`);
-console.log(`Expected: -0.75, Actual: ${divide(3, -4)}`);
-console.log(`Expected: 0.75, Actual: ${divide(-3, -4)}`);
-console.log(`Expected: Really bruh?, Actual: ${divide(3, 0)}`);
-console.log(`Expected: Really bruh?, Actual: ${divide(-3, 0)}`);
